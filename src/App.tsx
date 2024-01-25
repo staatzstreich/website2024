a2d8b67710f8bcbc7468ef49866f15cc9bcc958f
Michael Staatz <michael@staatzstreich.de>
2024-01-25 18:58:21 +0100

    Updated dependencies and removed some files

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
 import { FC } from "react";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import NoMatch from "./pages/NoMatch";
 import Weihnachtsgruss from "./pages/Weihnachtsgruss";
 
 const App: FC = () => {
-  return (
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path="weihnachtsgruss" element={<Weihnachtsgruss />} />
-      <Route path="*" element={<NoMatch />} />
-    </Routes>
-  );
+  return <Weihnachtsgruss />;
 };
 
 export default App;
