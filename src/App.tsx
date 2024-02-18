f8f6f9aff08bf023d1e58e52cc20eaad2aa60041
Michael Staatz <michael@staatzstreich.de>
2024-02-18 13:10:24 +0100

    Revert "Updated dependencies and removed some files"
    This reverts commit a2d8b67710f8bcbc7468ef49866f15cc9bcc958f.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { FC } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NoMatch from "./pages/NoMatch";
 import Weihnachtsgruss from "./pages/Weihnachtsgruss";
 
 const App: FC = () => {
-  return <Weihnachtsgruss />;
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="weihnachtsgruss" element={<Weihnachtsgruss />} />
+      <Route path="*" element={<NoMatch />} />
+    </Routes>
+  );
 };
 
 export default App;
